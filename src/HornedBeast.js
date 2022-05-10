befcb40f0db398b25def4148665e24c9e90f6793
Guy Farley <guy.e.farley@gmail.com>
2022-05-10 14:40:19 -0700

    Added feature 1 and feature 2

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import './HornedBeast.css';
 
 class HornedBeast extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicks: 0
+    }
+  }
+
+  clickPhoto = () => {
+    this.setState({
+      clicks: this.state.clicks + 1,
+    })
+  }
+
   render() {
     return (
       <article>
         <h2>{this.props.title}</h2>
-        <img src={this.props.image_url} alt={this.props.description} title={this.props.title} />
+        <p>❤️ favorited {this.state.clicks} times!</p>
+        <img src={this.props.image_url} alt={this.props.description} title={this.props.title} onClick={this.clickPhoto} />
         <p>{this.props.description}</p>
       </article>
     );
