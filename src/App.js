8eff14d5f1c9eeac7709a1d634391d8414bfb9a2
Guy Farley <guy.e.farley@gmail.com>
2022-05-12 13:41:27 -0700

    Adds SelectedBeast component to contain Modal

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import Header from './Header.js';
 import Main from './Main.js';
 import Footer from './Footer.js';
 import data from './data.json';
-import Modal from 'react-bootstrap/Modal';
 
 import './App.css';
+import SelectedBeast from './SelectedBeast.js';
 
 class App extends React.Component {
 
@@ -42,13 +42,12 @@ class App extends React.Component {
           openModalHandler={this.openModalHandler}
         />
         <Footer />
-        <Modal
-          size="lg"
-          show={this.state.isModalDisplaying}
-          onHide={this.closeModalHandler}>
-          <Modal.Body><img src={this.state.beastPic} alt={this.props.description} title={this.props.title} /></Modal.Body>
-          <Modal.Footer>{this.state.beastDesc}</Modal.Footer>
-        </Modal>
+        <SelectedBeast
+          closeModalHandler={this.closeModalHandler}
+          isModalDisplaying={this.state.isModalDisplaying}
+          beastPic={this.state.beastPic}
+          beastDesc={this.state.beastDesc}
+        />
       </>
     );
   }
